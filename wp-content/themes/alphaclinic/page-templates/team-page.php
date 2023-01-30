d4e79d39f2d9992e88ed7c0999371aa1ec2184df
devgalitein <dev.galitein@gmail.com>
2023-01-30 19:14:52 +0530

    team page template added.

---
+++ wp-content/themes/alphaclinic/page-templates/team-page.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: Team template
+ *
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+get_header(); ?>
+
+<div id="content-full" class="content-area">
+    <main id="main" class="site-main full-width" role="main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post();
+                $header_img = image_details(CFS()->get( 'header_background_image' ));
+                $header_title = CFS()->get( 'header_title' );
+                ?>
+                <div class="container-fluid">
+                    <div class="team-banner-section">
+                        <div class="team-header-bg">
+                            <?php echo '<img src="' . $header_img['url'][0] . '" alt="' . $header_img['alt'] . '" />'; ?>
+                        </div>
+                        <div class="team-text">
+                            <div class="container">
+                                <div class="team-text-inner">
+                                   <?php
+                                    if ($header_title) {
+                                        echo '<h3>'.$header_title.'</h3>';
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <?php get_template_part( 'template-parts/content', 'page' ); ?>
+                </div>
+            <?php endwhile; ?>
+
+        <?php else : ?>
+
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+        <?php endif; ?>
+
+    </main><!-- #main -->
+</div><!-- #content-full -->
+
+<?php get_footer(); ?>
