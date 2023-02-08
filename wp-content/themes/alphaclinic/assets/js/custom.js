08546f2f4545fe5cea8be4061983fc3a09621d8e
devgalitein <dev.galitein@gmail.com>
2023-02-08 10:53:32 +0530

    joint detail page scroll to the section js added.

--- wp-content/themes/alphaclinic/assets/js/custom.js
+++ wp-content/themes/alphaclinic/assets/js/custom.js
@@ -6,4 +6,15 @@ jQuery(document).ready(function ($) {
         arrows: false,
         adaptiveHeight: true,
     });
+
+    $(".joint-details-page-header .header-right-text ul li a").click(function(e) {
+        var href = $(this).attr('href');
+        if (href) {
+            var body = $("html, body");
+            body.stop().animate({scrollTop: jQuery(href).offset().top - 80}, 100, 'swing', function () {
+            });
+        }
+    })
+
 });
+
