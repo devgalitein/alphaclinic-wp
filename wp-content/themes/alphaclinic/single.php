e4cfc2b6e03013a8ee533377667290a80711b495
devgalitein <dev.galitein@gmail.com>
2023-03-06 11:33:55 +0530

    single post page unnecessary data removed.

--- wp-content/themes/alphaclinic/single.php
+++ wp-content/themes/alphaclinic/single.php
@@ -20,7 +20,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 get_header(); ?>
 
-	<div id="content" class="content-area">
+	<div id="content" class="content-area container">
 			<main id="main" class="site-main" role="main">
 
 				<?php get_template_part( 'template-parts/loop-header' ); ?>
@@ -28,17 +28,6 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-					<?php responsive_mobile_post_nav(); ?>
-
-					<?php responsive_mobile_comments_before(); ?>
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || '0' != get_comments_number() ) :
-							comments_template( '', true );
-						endif;
-					?>
-					<?php responsive_mobile_comments_after(); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
